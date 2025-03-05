2502fdab5817cdb24c72ff6b129d86d8c24cced6
osledgecar <oscar.xu25@stgeorges.bc.ca>
2025-03-05 09:26:44 -0800

    protoype progress

--- List.js
+++ List.js
@@ -1,7 +1,8 @@
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import { CheckBox } from 'react-native-elements';
 import Task from './Task';
+
 
 function List(props){
     const [checked, toggleCheck] = useState(false);
@@ -9,32 +10,27 @@ function List(props){
     const toggle = () => {
         toggleCheck(!checked);
     };
-    if (!checked){
-        let show = (<FlatList
-            display={disp}
-            data={props.items}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-            <Task name={item.name} onDelete={props.deleteItem} id={item.id} />
-            )}
-        />);
-    } else {
-        let show;
-    }
+    
     return (
         <View style={styles.list}>
-            <CheckBox
-                      checked={checked}
-                      onPress={toggle}
-                      containerStyle={styles.checkbox}
-                    />
-            <Text style={styles.header}>{props.header}</Text>
-            
-            {show}
-            
+          <CheckBox
+            checked={checked}
+            onPress={toggle}
+            containerStyle={styles.checkbox}
+          />
+          <Text style={styles.header}>{props.header}</Text>
+          {!checked && (
+            <FlatList
+              data={props.items}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <Task name={item.name} onDelete={props.deleteItem} id={item.id} />
+              )}
+            />
+          )}
         </View>
-        
-    );
+      );
+      
 
 }
 
